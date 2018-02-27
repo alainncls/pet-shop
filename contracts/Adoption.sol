0aea60d03d1cf89574dec006b13483dbd72ab2a6
Alain NICOLAS <alain.nicolas@talan.com>
2018-02-27 18:46:26 +0100

    Complete Adoption contract

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -3,9 +3,20 @@ pragma solidity ^0.4.17;
 contract Adoption {
 
     // Adopters array
+    address[16] public adopters;
 
     // Method to adopt pet
+    function adopt(uint petId) public returns (uint) {
+      require(petId >= 0 && petId <= 15);
+
+      adopters[petId] = msg.sender;
+
+      return petId;
+    }
 
     // Method to get adopters
+    function getAdopters() public view returns (address[16]) {
+      return adopters;
+    }
 
 }
